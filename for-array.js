ce1106bf62892bcca1d894cbc0369260851e52ad
PeuGomes97 <peugomes97@gmail.com>
2023-07-25 10:29:24 -0300

    test new branch

--- for-array.js
+++ for-array.js
@@ -186,14 +186,7 @@ return arr.filter(function(val){
 });
 }
 
-/*
-Write a function called removeVowels which accepts a string and returns a new string with all of the vowels (both uppercased and lowercased) removed. Every character in the new string should be lowercased.
 
-Examples:
-    removeVowels('Elie') // ('l')
-    removeVowels('TIM') // ('tm')
-    removeVowels('ZZZZZZ') // ('zzzzzz')
-*/
 
 function removeVowels(str) {
   const vowels = "aeiou";
